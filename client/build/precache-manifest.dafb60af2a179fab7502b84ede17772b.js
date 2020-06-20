677610c7fdf2a2181075234f362001d31ff7d0ee
Evgeniy Grinko <john_forever_young@mail.ru>
2020-06-20 23:46:32 +0300

    Adding build dir

---
+++ client/build/precache-manifest.dafb60af2a179fab7502b84ede17772b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ae48625038a1dcfc38e788b5fc3ec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0f3b23c88a96e42713",
+    "url": "/static/js/2.9c917508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c917508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6234bf707ab176ff804e",
+    "url": "/static/js/main.c7676206.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
