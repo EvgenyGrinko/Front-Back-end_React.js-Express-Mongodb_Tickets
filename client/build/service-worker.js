61b27b25d1376f4194ea5d39a5d79e266446a6e9
Evgeniy Grinko <john_forever_young@mail.ru>
2020-06-25 14:15:57 +0300

    Add build folder

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dafb60af2a179fab7502b84ede17772b.js"
+  "/precache-manifest.70979cde160dd0c30e8e1dc7477d420c.js"
 );
 
 self.addEventListener('message', (event) => {
